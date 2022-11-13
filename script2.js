afe6929ca68c1db98c2abe8de80cb135d180b04e
babykrishnarayaguru <babykrishnarayaguru@gmail.com>
2022-11-14 00:24:39 +0530

    firstcommit

---
+++ script2.js
@@ -0,0 +1,17 @@
+const BG_IMG = new Image();
+BG_IMG.src = "img/bg.jpg";
+
+const SCORE_IMG = new Image();
+SCORE_IMG.src = "img/score.png";
+
+const LIFE_LOST = new Audio();
+LIFE_LOST.src = "sounds/lost.mp3";
+
+const WIN = new Audio();
+WIN.src = "sounds/win.mp3";
+
+const BRICK_HIT = new Audio();
+BRICK_HIT.src = "sounds/hit.mp3";
+
+const music = new Audio();
+music.src = "sounds/music.mp3";
